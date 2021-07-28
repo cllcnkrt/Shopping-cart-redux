7613fb95bb7d14e7ce89a81b71a0890222dad3ba
cllcnkrt <cllcnkrt@outlook.com>
2021-07-28 15:02:07 +0300

    project created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React from "react";
+import "./styles.css";
+import { Route } from "react-router-dom";
 
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
-function App() {
+import { data } from "./data";
+
+export default function App() {
   return (
     <div className="App">
+      <h1>
+      <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+        Shopping Cart
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+      </h1>
+      <Route exact path="/" component={Products} />
+      <Route path="/cart" component={Cart} />
     </div>
   );
 }
-
-export default App;
